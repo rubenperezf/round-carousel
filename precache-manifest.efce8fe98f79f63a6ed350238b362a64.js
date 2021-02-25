4976989b1ff9f678963804773060cd5b97e90a67
rubenperezf <rubenperezfeijoo@gmail.com>
2021-02-25 15:20:07 -0500

    Updates

---
+++ precache-manifest.efce8fe98f79f63a6ed350238b362a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656ca91549498aff66d9ddbde2b85dda",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc60a0125ea368ddf003",
+    "url": "./static/css/main.f9f67d48.chunk.css"
+  },
+  {
+    "revision": "f60b6dc90577c12ea1dc",
+    "url": "./static/js/2.db988ee6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.db988ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc60a0125ea368ddf003",
+    "url": "./static/js/main.25a1ead6.chunk.js"
+  },
+  {
+    "revision": "468596de4dbe5e46ebaf",
+    "url": "./static/js/runtime-main.433cbfa7.js"
+  }
+]);
