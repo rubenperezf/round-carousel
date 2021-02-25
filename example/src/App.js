9130b9e53e6ea429704cf1c94af228f6ff1d62a3
rubenperezf <rubenperezfeijoo@gmail.com>
2021-02-25 15:16:13 -0500

    some changes

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,97 @@
 import React from 'react'
 
-import { ExampleComponent } from 'round-carousel'
+import { RoundCarousel } from 'round-carousel'
 import 'round-carousel/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const list = [
+    {
+      title: 'Lugo',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/8/8d/Muralla_Catedral_Lugo.jpg',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'London',
+      image:
+        'https://www.fodors.com/assets/destinations/2869/tower-bridge-london-england_980x650.jpg',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'Charleston',
+      image:
+        'https://www.counton2.com/wp-content/uploads/sites/7/2020/09/Charleston-Penninsula-with-Ravenel-Bridge.jpg?w=1920&h=1080&crop=1',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'Sidney',
+      image:
+        'https://i.pinimg.com/originals/da/72/25/da7225db7c96e00e5ffe3e5ea8dfa054.jpg',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'Moscow',
+      image:
+        'https://aic.azureedge.net/pgl-release/Images/ArticleImages/19/19712.jpg?v=15',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'Madrid',
+      image:
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/33/e6/bf/caption.jpg?w=1000&h=-1&s=1',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'Paris',
+      image:
+        'https://photos.mandarinoriental.com/is/image/MandarinOriental/paris-2017-home?wid=2880&hei=1280&fmt=jpeg&crop=9,336,2699,1200&anchor=1358,936&qlt=75,0&fit=wrap&op_sharpen=0&resMode=sharp2&op_usm=0,0,0,0&iccEmbed=0&printRes=72',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'Venice',
+      image:
+        'https://expertvagabond.com/wp-content/uploads/venice-italy-highlights-guide.jpg',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'Tokio',
+      image:
+        'https://miviaje.com/wp-content/uploads/2018/12/tokio-ciudad-segura.jpg',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'San Francisco',
+      image:
+        'https://qtxasset.com/styles/breakpoint_sm_default_480px_w/s3/travelagentcentral/1597853698/GettyImages-1136437406.jpg/GettyImages-1136437406.jpg?J3neYunxLcKFuuBDV7tatFNIawH6Ascs&itok=TBkxtz0m',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'Barcelona',
+      image:
+        'https://cdn-image.departures.com/sites/default/files/1559596629/sagrada-familia-barcelona-BARCELONA0619.jpg',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    },
+    {
+      title: 'New York',
+      image:
+        'https://blog-www.pods.com/wp-content/uploads/2019/04/MG_1_1_New_York_City-1.jpg',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, '
+    }
+  ]
+
+  return <RoundCarousel list={list} width={600} timer={5000} />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,113 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const RoundCarousel = ({ list, width, timer = 2000, color = [] }) => {
+  const [showWord, setShowWord] = useState([list[11]])
+  const [number, setNumber] = useState(0)
+  let selectedWidth = width
+
+  let slide
+  useEffect(() => {
+    if (number !== 100) {
+      slide = setTimeout(() => {
+        setShowWord(list[number])
+        if (number === 0 || number <= 10) {
+          setNumber(number + 1)
+        } else {
+          setNumber(0)
+        }
+      }, timer)
+    }
+  }, [number])
+
+  let whatColor = (x, i) => {
+    if (showWord.title === x.title && i == 0) {
+      return color.length > 0 ? color[0] : '#6ee9fa'
+    } else if (showWord.title === x.title && i == 1) {
+      return color.length > 0 ? color[1] : '#14deff'
+    } else if (showWord.title === x.title && i == 2) {
+      return color.length > 0 ? color[2] : '#00d2ff'
+    } else if (showWord.title === x.title && i == 3) {
+      return color.length > 0 ? color[3] : '#00c5ff'
+    } else if (showWord.title === x.title && i == 4) {
+      return color.length > 0 ? color[4] : '#00b8ff'
+    } else if (showWord.title === x.title && i == 5) {
+      return color.length > 0 ? color[5] : '#00aaff'
+    } else if (showWord.title === x.title && i == 6) {
+      return color.length > 0 ? color[6] : '#009aff'
+    } else if (showWord.title === x.title && i == 7) {
+      return color.length > 0 ? color[7] : '#0089ff'
+    } else if (showWord.title === x.title && i == 8) {
+      return color.length > 0 ? color[8] : '#0077ff'
+    } else if (showWord.title === x.title && i == 9) {
+      return color.length > 0 ? color[9] : '#0061ff'
+    } else if (showWord.title === x.title && i == 10) {
+      return color.length > 0 ? color[10] : '#0045ff'
+    } else if (showWord.title === x.title && i == 11) {
+      return color.length > 0 ? color[11] : '#480df9'
+    } else {
+      return null
+    }
+  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <div style={{ width: '60%' }}>
+        <ul
+          className={styles.circle}
+          style={{
+            position: 'relative',
+            width: `${selectedWidth}px`,
+            height: `${selectedWidth}px`,
+            margin: '0 auto'
+          }}
+        >
+          {list.map((x, i) => {
+            return (
+              <li
+                key={i}
+                onClick={() => {
+                  setShowWord(list[i])
+                  setNumber(100)
+                  clearTimeout(slide)
+                }}
+              >
+                <div
+                  className={
+                    showWord.title === x.title ? styles.text : styles.activeText
+                  }
+                  style={{ backgroundColor: whatColor(x, i) }}
+                >
+                  {i}
+                </div>
+              </li>
+            )
+          })}
+          <img
+            style={{
+              position: 'absolute',
+              top: '17%',
+              left: '17%',
+              borderRadius: '100%',
+              width: `${selectedWidth - selectedWidth / 3}px`,
+              height: `${selectedWidth - selectedWidth / 3}px`,
+              zIndex: -1
+            }}
+            src={showWord.image}
+          />
+        </ul>
+      </div>
+      <div style={{ width: '40%', margin: '20px' }}>
+        <p style={{ fontSize: '50px', textAlign: 'center' }}>
+          {showWord.title}
+        </p>
+        <p style={{ fontSize: '20px' }}>{showWord.text}</p>
+      </div>
+    </div>
+  )
 }
